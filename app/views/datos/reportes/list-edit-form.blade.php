4370a7ef614da23206b4153424c682f2a79e61ab
luismen31 <luis2531@gmail.com>
2015-08-06 20:06:35 -0500

    vista reportes y cambio en estilo css

--- app/views/datos/reportes/list-edit-form.blade.php
+++ app/views/datos/reportes/list-edit-form.blade.php
@@ -12,4 +12,35 @@
   <center>Reportes</center>
 </h1><hr>
 
+<div class="portlet">
+	<div class="tabbable-panel">
+		<div class="tabbable-line">			
+			<ul class="nav nav-tabs ">
+				<li class="active">
+					<a href="#tab1" data-toggle="tab">
+					Más costosos </a>
+				</li>
+				<li>
+					<a href="#tab2" data-toggle="tab">
+					Act. con más fallas </a>
+				</li>
+				<li>
+					<a href="#tab3" data-toggle="tab">
+					Garantías </a>
+				</li>
+			</ul>
+			<div class="tab-content">
+				<div class="tab-pane fade in active" id="tab1">
+					tab 1
+				</div>
+				<div class="tab-pane fade" id="tab2">
+					tab 2
+				</div>
+				<div class="tab-pane fade" id="tab3">
+					tab 3
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @stop
